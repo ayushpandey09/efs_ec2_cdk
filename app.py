24226c6e553f8e35fa2c1701ba748b585dab4405
EC2 Default User <ec2-user@ip-172-31-12-181.ec2.internal>
2024-03-18 14:35:28 +0000

    first commit

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ Ec2EfsDemoStack(app, "Ec2EfsDemoStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account='058264506183', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
